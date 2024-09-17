4839c80adc446961333da71c90f165fcae4557d8
Amisha Jha <amishajha06@gmail.com>
2024-09-17 11:28:39 +0530

    About, cover and intro added

--- src/App.js
+++ src/App.js
@@ -2,25 +2,30 @@ import './App.css';
 import Header from './components/header/header';
 import Counter from './components/counter/counter';
 import TodoList from './components/to-do/to-do';
+import About from './components/about/about';
+
 
 
 
 function App() {
-  
+
 
   return (
     <div className="App">
-      <div className='header'>
-      <Header/>
-      </div>
-     
-     <div>
-     <Counter/>
 
-     </div>
-     <div>
-      <TodoList/>
-     </div>
+      <div className='header'>
+        <Header />
+      </div>
+      <div>
+        <About />
+      </div>
+      <div>
+        <Counter />
+
+      </div>
+      <div>
+        <TodoList />
+      </div>
     </div>
   );
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import cover from '../../images/Cover.png';
 
 function Header() {
-
-
   return (
     <div>
-      <h1>My ReactJs Projects</h1>
+      <img src={cover} alt='cover' />
+      <p>
+        Experienced professional with 3.5 years in the corporate sector, including 1.8 years of focused experience in
+        frontend development. Skilled in collaborating with cross-functional teams to plan and execute functional
+        specifications for websites and applications. Proficient in building multi-platform e-commerce websites
+        utilizing Responsive Web Design and CSS frameworks.
+      </p>
       
     </div>
+
   );
 }
 
