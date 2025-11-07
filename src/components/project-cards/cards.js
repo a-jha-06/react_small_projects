f4d1fd470d4652a0743c1051cf2fa2e853964613
AMISHA JHA <106396122+a-jha-06@users.noreply.github.com>
2025-11-07 15:45:44 +0530

    Add Palmonas project card to the cards component
    Added a new project card for Palmonas with details and a link.

--- src/components/project-cards/cards.js
+++ src/components/project-cards/cards.js
@@ -14,6 +14,20 @@ function Cards() {
         <div className='container'>
             <h2 className='heading'>Projects</h2>
             <div class="row row-cols-1 row-cols-md-2 g-4">
+
+        <div class="col">
+
+                    <div class="card">
+                        <img src='https://cdn.shopify.com/s/files/1/0613/1400/7297/files/Palmonas_Logo_Open_File-02.webp?v=1760074313/>
+                        <div class="card-body">
+                            <h5 class="card-title">Palmonas</h5>
+                            <p class="card-text">Palmonas is an Indian jewelry brand specializing in demi-fine jewelry,
+        which combines the affordability of fashion jewelry with the quality of fine jewelry.
+        The brand offers waterproof, anti-tarnish pieces made from high-quality base metals like surgical steel or Sterling silver 925, plated with 18k gold</p>
+                            <a href='https://palmonas.com/' class="button">Visit Website</a>
+                        </div>
+                    </div>
+                </div>
                 <div class="col">
 
                     <div class="card">
