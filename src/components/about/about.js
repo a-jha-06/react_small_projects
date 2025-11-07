acec164c3a5352a21e363ce4dbf6bbad18d04bc6
AMISHA JHA <106396122+a-jha-06@users.noreply.github.com>
2025-11-07 15:50:00 +0530

    Update professional summary and current position

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -8,12 +8,11 @@ function About() {
             <h2 className="heading">Professional Summary</h2>
             <div className="list">
                 <p className="list-item">
-                    I am a Lead Shopify Developer with 3.5 years of corporate experience, specializing in frontend development using React.js, Next.js, JavaScript, and Shopify. With a strong foundation in e-commerce website development, I have successfully built and optimized custom Shopify themes, integrated third-party APIs, and enhanced user experiences to drive conversions.
+                    I am a Lead Shopify Developer with 4+ years of corporate experience, specializing in frontend development using React.js, React Native, Next.js, JavaScript, and Shopify Plus. With a strong foundation in e-commerce website development, I have successfully built and optimized custom Shopify themes, integrated third-party APIs,handled POS operations and enhanced user experiences to drive conversions.
 
                     My expertise extends to performance optimization, SEO strategies, and modern UI/UX design, ensuring seamless and engaging online shopping experiences. I am passionate about problem-solving, collaboration, and delivering scalable solutions that contribute to business growth.
 
-                    Currently working at Titbit Foods, I am seeking opportunities to leverage my skills in Shopify development, frontend technologies, and e-commerce innovation.
-                </p>
+                    Currently working at Palmonas, I’m looking forward to connecting and discussing how we can solve modern-day eCommerce challenges through innovative Shopify and frontend solutions.            </p>
             </div>
 
         </div>
